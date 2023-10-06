b6e1bae75b9e34d0027e3beb7787c08755d50159
Tomas <tomerl@tomasbrazao.com>
2023-10-07 00:34:00 +0100

    Added redirect for dashboard if there is no user in session

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard')]
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
+        if (!$session->has('user')) {
+            return $this->redirectToRoute('login');
+        }
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'controller_name' => 'DashboardController'
         ]);
     }
 }
